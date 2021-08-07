609e850ea83eacd825b5f649e05ae59dfdac3f47
Valentin <ValentinVoznyuk@gmail.com>
2021-08-08 00:41:34 +0300

    first release

---
+++ index.js
@@ -0,0 +1,6 @@
+const eslintrc = require('./lib/typescript-next-app/.eslintrc.json');
+const pkg = require('./package');
+
+console.log(`(͠≖ ͜ʖ͠≖) ${pkg.name}:${pkg.version}`);
+
+module.exports = eslintrc;
